32e5932bd9de8d46b2b6f912cce4dcefbc7e05d6
suresh C <sureshs68167@gmail.com>
2026-02-27 14:50:49 +0530

    Update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -111,9 +111,9 @@ export default function AboutPage() {
               <h4 className="text-xl font-semibold mb-2">
                 Academic Background
               </h4>
-
+               <b>MBA</b> 
               <p>
-                <b>MBA</b> 
+               
                 <b>M.A </b>- ( English Literature )  </p>
 
             </div>
